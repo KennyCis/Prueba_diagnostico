749dba8ceebbf4e215fa9a215755d649ac958f56
KennyCis <maxpropvp2@gmail.com>
2025-10-27 19:33:40 -0500

    Add HTML structure to main.py
    HICE UNA ACTUALIZACION

---
+++ main.py
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>PRUEBA DIAGNÓSTICA</title>
+</head>
+<body>
+
+    <h1>WEB PROGRAMMING</h1>
+
+    <p>My first paragraph.</p>
+    <div class="card" style="width: 18rem;">
+       
+        <img src="OIP.webp" class="card-img-top" alt="UCE">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+
+
+    <table class="table mt-4">
+        <thead class="table-dark">
+            <tr>
+                <th scope="col">Number</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+            </tr>
+        </tbody>
+    </table>
+
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+</body>
+
+
+</html>
