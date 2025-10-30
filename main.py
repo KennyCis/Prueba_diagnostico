10c4c96c538e70f2f3dfa5420ac66e1b2fbc150c
KennyCis <maxpropvp2@gmail.com>
2025-10-29 22:50:24 -0500

    Replace HTML content with FastAPI application
    Actualice mi backend  con api de rick and morty

--- main.py
+++ main.py
@@ -1,64 +1,25 @@
-<!DOCTYPE html>
-<html lang="es">
+from fastapi import FastAPI
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+import requests 
+from fastapi.middleware.cors import CORSMiddleware
 
-    <title>PRUEBA DIAGNÓSTICA</title>
-</head>
-<body>
+app = FastAPI()
 
-    <h1>WEB PROGRAMMING</h1>
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-    <p>My first paragraph.</p>
-    <div class="card" style="width: 18rem;">
-       
-        <img src="OIP.webp" class="card-img-top" alt="UCE">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-    </div>
+url= "https://rickandmortyapi.com/api/character"
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-
-    <table class="table mt-4">
-        <thead class="table-dark">
-            <tr>
-                <th scope="col">Number</th>
-                <th scope="col">Name</th>
-                <th scope="col">Description</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-            </tr>
-        </tbody>
-    </table>
-
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-
-
-
-
-</body>
-
-
-</html>
+@app.get("/character/{id}")
+def get_character(id:int):
+    result = requests.get(f"{url}/{id}")
+    print (result) 
+    return result.json()
